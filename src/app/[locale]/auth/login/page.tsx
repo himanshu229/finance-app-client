f140733cdd2017f778b9e8aa46ed3c633c3ce8e1
himanshu <himanshu.s2299@gmail.com>
2024-01-03 17:01:52 +0530

    fix the css

--- src/app/[locale]/auth/login/page.tsx
+++ src/app/[locale]/auth/login/page.tsx
@@ -1,11 +1,16 @@
 "use client";
-import { useRouter } from '@/navigation';
-import { Container } from '@mui/material';
-import React from 'react'
+import { useRouter } from "@/navigation";
+import { Box } from "@mui/material";
+import React from "react";
 
 export default function Login() {
-  const navigate = useRouter()
-  return <Container>
-  <button onClick={()=>navigate.push("/")}>{"back"}</button>
-  </Container>;
+  const navigate = useRouter();
+  return (
+    <Box className="min-h-screen flex items-center justify-center bg-gradient-to-bl from-darkBlue via-lighterBlue to-lightestBlue  text-white">
+      <Box className="border-2 bg-white w-1/2 h-96 rounded-xl md:flex gap-4 p-6">
+        <Box className="text-black w-24">Hello</Box>
+        <Box className="text-black">hello</Box>
+      </Box>
+    </Box>
+  );
 }
