1a6f0db29bf8ee14d42d5fe2c4bedddcb71a3a8c
himanshu <himanshu.s2299@gmail.com>
2024-01-06 02:42:16 +0530

    react setup

--- src/app/[locale]/auth/login/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-const LoginPage = dynamic(() => import("@/pages/authentication/login"));
-export default function Login() {
-  return <LoginPage />;
-}
--- src/assests/images/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as IMG_Rupess} from "./rupee-icon.jpg"
